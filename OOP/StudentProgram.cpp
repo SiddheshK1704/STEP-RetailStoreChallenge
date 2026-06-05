48766877b433d4895f5a84adb83622eb08b27df7
SiddheshK1704 <siddheshkhankhoje@gmail.com>
2026-06-05 12:50:23 +0530

    Updated OOP/StudentProgram.cpp

--- OOP/StudentProgram.cpp
+++ OOP/StudentProgram.cpp
@@ -42,7 +42,7 @@ int main() {
     cout<<"Enter the name of the Student with their 3 scores:\n";
     cin >> name >> s1 >> s2 >> s3;
 
-    if(s1>100 || s1<0 || s2>100 || s2<0 || s3>100 || s3<0){
+    if((s1>100 || s1<0) || (s2>100 || s2<0) || (s3>100 || s3<0)){
         cout<<"Invalid score entered. Scores must be between 0 and 100."<<endl;
     }
 
