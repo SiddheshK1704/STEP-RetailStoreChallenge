6c2fe26ea89a3c5b8a102ee026d4b1f5a100751a
SiddheshK1704 <siddheshkhankhoje@gmail.com>
2026-06-05 12:42:45 +0530

    Made minor required changes in StudentProgram.cpp

--- OOP/StudentProgram.cpp
+++ OOP/StudentProgram.cpp
@@ -38,11 +38,13 @@ public:
 int main() {
     string name;
     int s1, s2, s3;
+    
+    cout<<"Enter the name of the Student with their 3 scores:\n";
+    cin >> name >> s1 >> s2 >> s3;
+
     if(s1>100 || s1<0 || s2>100 || s2<0 || s3>100 || s3<0){
         cout<<"Invalid score entered. Scores must be between 0 and 100."<<endl;
     }
-    cout<<"Enter the name of the Student with their 3 scores:\n";
-    cin >> name >> s1 >> s2 >> s3;
 
     Student stu(name, s1, s2, s3);
 
